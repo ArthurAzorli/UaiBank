4e1bde7e93074dcc02a90359ba5e73b41f24d5b3
Azorli <arthurgagliardiazorli@gmail.com>
2025-04-10 22:03:03 -0300

    feat: add utils de IO

---
+++ util/io_util.c
@@ -0,0 +1,40 @@
+#include "io_util.h"
+#include <stdio.h>
+
+/**
+ * @brief: caso seja necessario, a função limpa o buffer de leitura do terminal
+ * @return: valor booleano indicando se foi preciso limpar (TRUE) ou não (FALSE)
+ */
+bool clearBuffer() {
+    int c = getchar();
+    if (c != '\n' && c != EOF) {
+        while ((c = getchar()) != '\n' && c != EOF){}
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief: exibe uma menssagem formatada de erro
+ * @param message: texto da mensagem exibida
+ */
+void error_message(char *message) {
+    printf("\x1b[31mErro: %s\n\e[0;37m", message);
+
+}
+
+/**
+ * @brief: exibe uma menssagem formatada de sucesso
+ * @param message: texto da mensagem exibida
+ */
+void successful_message(char *message) {
+    printf("\x1b[0;32m%s\n\e[0;37m", message);
+}
+
+/**
+ * @brief: espera o usuário pressionar enter
+ */
+void press_enter() {
+    printf("Pressione Enter para continuar...");
+    clearBuffer();
+}
---
+++ util/io_util.h
@@ -0,0 +1,14 @@
+#ifndef IO_UTIL_H
+#define IO_UTIL_H
+
+#include "int_util.h"
+
+bool clearBuffer();
+
+void error_message(char* message);
+
+void successful_message(char* message);
+
+void press_enter();
+
+#endif //IO_UTIL_H
